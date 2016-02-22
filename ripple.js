d3f8a58274c33a1b3d73d4608648788aad45921f
Jaon <jamonsterr@gmail.com>
2016-02-22 15:32:42 -0300

    cross browser init

--- ripple.js
+++ ripple.js
@@ -4,7 +4,7 @@
 
   if (!('$' in window)) window.$ = {};
 
-  var style = '.ripple{overflow:hidden; -webkit-mask-image: -webkit-radial-gradient(circle, white, black);}.ripple-mask{position:absolute;border-radius:50%;width:50px;height:50px;background:#fff;animation:ripple-animation 2s forwards;}@keyframes ripple-animation{from{transform:translate(-50%,-50%) scale(1);opacity:.4}to{transform:scale(100);opacity:0}}';
+  var style = '.ripple{-webkit-mask-image:-webkit-radial-gradient(circle,#fff,#000);overflow:hidden}.ripple-mask{position:absolute;border-radius:50%;width:50px;height:50px;background:#fff;-webkit-animation:ripple-animation 2s forwards;-moz-animation:ripple-animation 2s forwards;-o-animation:ripple-animation 2s forwards;animation:ripple-animation 2s forwards}@-webkit-keyframes ripple-animation{from{transform:translate(-50%,-50%) scale(1);opacity:.4}to{transform:scale(100);opacity:0}}@-moz-keyframes ripple-animation{from{transform:translate(-50%,-50%) scale(1);opacity:.4}to{transform:scale(100);opacity:0}}@-o-keyframes ripple-animation{from{transform:translate(-50%,-50%) scale(1);opacity:.4}to{transform:scale(100);opacity:0}}@keyframes ripple-animation{from{transform:translate(-50%,-50%) scale(1);opacity:.4}to{transform:scale(100);opacity:0}}';
   var styleEl = document.createElement('style');
   styleEl.innerText = style;
   document.head.appendChild(styleEl);
